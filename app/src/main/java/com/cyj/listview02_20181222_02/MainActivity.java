df04f13b4f6b3a2a5321af479d172031a14cb407
test <test1>
2018-12-22 15:08:06 +0900

    임시 데이터 삽입 코드

--- app/src/main/java/com/cyj/listview02_20181222_02/MainActivity.java
+++ app/src/main/java/com/cyj/listview02_20181222_02/MainActivity.java
@@ -30,6 +30,21 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupValues() {
+        fillStores();
+
+    }
+
+//    가게 데이터를 채우기 위한 임시 메소드
+    void fillStores(){
+        storeList.clear();
+
+        storeList.add(new Store("BBQ", "종로구 10길", 4, "오후2시 ~ 오전1시", "01012345678"));
+        storeList.add(new Store("네네치킨", "종로구 7길", 4, "오후3시 ~ 오전12시", "01098765432"));
+        storeList.add(new Store("노랑통닭", "종로구 15길", 4, "오후1시 ~ 오전1시", "01012345678"));
+        storeList.add(new Store("디디치킨", "종로구 8길", 4, "오후4시 ~ 오전3시", "01098765432"));
+        storeList.add(new Store("BHC", "종로구 1길", 4, "오후12시 ~ 오전12시", "01012345678"));
+        storeList.add(new Store("교촌치킨", "종로구 2길", 4, "오후2시 ~ 오전2시", "01098765432"));
+        storeList.add(new Store("굽네치킨", "종로구 4길", 4, "오후3시 ~ 오전2시", "01012345678"));
 
     }
 
