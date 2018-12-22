d7025297aab837d2c31fb37282cd1ab0666ca86e
test <test1>
2018-12-22 15:44:41 +0900

    가계 상세화면

---
+++ app/src/main/java/com/cyj/listview02_20181222_02/StoreDetailActivity.java
@@ -0,0 +1,31 @@
+package com.cyj.listview02_20181222_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoreDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_detail);
+        bindViews();
+        setupEvents();
+        setupValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setupValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
